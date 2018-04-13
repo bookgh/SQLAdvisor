c03c164bb56bbd4b3d722dc647f59409506a072b
ppabc8 <ppabc8@gmail.com>
2018-04-13 10:49:50 +0800

    update

--- php-sqlreview/run.sh
+++ php-sqlreview/run.sh
@@ -6,5 +6,4 @@ service httpd  start
 echo "" | mysqladmin -uroot -p password 111111
 mysql -uroot -p"111111" -e "CREATE DATABASE IF NOT EXISTS sql_db CHARACTER SET utf8;"
 mysql -uroot -D sql_db -p"111111" < "/php-sqlreview/dbinfo.sql.txt"
-mysql -uroot -D sql_db -p"111111" < "/php-sqlreview/operation.sql.txt"
 ping 127.0.0.1 >> /dev/null
